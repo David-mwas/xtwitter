1831f898496dd0daeb019d628076ffc95f59e03a
David-mwas <dmwas704@gmail.com>
2025-07-01 19:05:51 +0300

    ADD(CORE): wrap connectDb,to async func

--- backened/src/server.js
+++ backened/src/server.js
@@ -3,9 +3,20 @@ import { connectDB } from "./config/db.js";
 import { ENV } from "./config/env.js";
 
 const app = express();
-connectDB();
 
 app.get("/", (req, res) => res.send("xtwitter, says hello world"));
 
 const PORT = ENV.PORT || 5000;
-app.listen(PORT, () => console.info(`server running on http://localhost:${PORT}`));
+const startSever = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () =>
+      console.info(`server running on http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.error("Error", error);
+    process.exit(1);
+  }
+};
+
+startSever();
