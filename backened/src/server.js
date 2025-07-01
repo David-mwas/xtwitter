347a9e2d9914d9e1ff68a5aefaa95c45d1921738
David-mwas <dmwas704@gmail.com>
2025-07-01 22:00:03 +0300

    ADD(CORE): add post,routes,controllers for comments

--- backened/src/server.js
+++ backened/src/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
 const app = express();
 
@@ -18,9 +19,10 @@ app.get("/", (req, res) => res.send("xtwitter, says hello world"));
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/comments", commentRoutes);
 
 // error handling
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error("Unhandled error:", err);
   res.status(500).json({ error: err.message || "Internal server error" });
 });
